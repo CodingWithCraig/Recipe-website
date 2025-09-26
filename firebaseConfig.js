1ca3b4e09004a16a196df85ec3952279c3738378
Craig Hlongwani <clkhlongwani2@gmail.com>
2025-09-26 21:37:35 +0200

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,1 +1,10 @@
-
+// ---------- Firebase Config ----------
+const firebaseConfig = {
+    apiKey: "AIzaSyBPqkgBAQwhCMTUsrHcd5vct9FBNAxJQ50",
+    authDomain: "finerecip-website.firebaseapp.com",
+    projectId: "finerecip-website",
+    storageBucket: "finerecip-website.appspot.com",
+    messagingSenderId: "1081847047055",
+    appId: "1:1081847047055:web:8c60c5e2c0c6a6766aa1fa",
+    measurementId: "G-RJ77SR3CMB"
+};
