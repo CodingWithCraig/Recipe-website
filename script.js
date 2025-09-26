866b20416183c3896c17eaaa90ffc86eb55b178b
Craig Hlongwani <clkhlongwani2@gmail.com>
2025-09-26 22:07:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,15 @@ import { getFirestore, collection, addDoc, getDocs }
 
 
 // ---------- Firebase Config ----------
-import {firebaseConfig} from './firebaseConfig.js';
+const firebaseConfig = {
+    apiKey: "AIzaSyBPqkgBAQwhCMTUsrHcd5vct9FBNAxJQ50",
+    authDomain: "finerecip-website.firebaseapp.com",
+    projectId: "finerecip-website",
+    storageBucket: "finerecip-website.appspot.com",
+    messagingSenderId: "1081847047055",
+    appId: "1:1081847047055:web:8c60c5e2c0c6a6766aa1fa",
+    measurementId: "G-RJ77SR3CMB"
+};
 
 // ---------- Initialize Firebase ----------
 const app = initializeApp(firebaseConfig);
